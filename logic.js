6c3cf9c18ff0c8cc762f4deec71bdc5b2db1b3c3
salil sharma <salilsharma562@gmail.com>
2022-06-24 16:29:38 +0530

    Added more features to the game

--- logic.js
+++ logic.js
@@ -2,9 +2,12 @@ let startButton = document.getElementById('startGame');
 let computer = document.getElementById('computer');
 let textarea = document.getElementById('textarea');
 let alert=document.getElementById('alert');
+let userChance=document.getElementById('chance');
+let chance=10;
 startButton.addEventListener('click', () => {
     let comp = Math.floor((Math.random() * 100) + 1);
     textarea.innerHTML = `<input type="text" id="userInput" placeholder="Choose your number"></input></br><button class="btn btn-primary" id="choose">Choose</button>`;
+    userChance.innerHTML=`<p>Chance left : <b>${chance}</b>`;
     startButton.classList.remove('btn-primary');
     startButton.classList.add('btn-success');
     let comHtml = `<p>Computer choosen its's number</br>Please predict the number choosen by computer</br>to win the game`;
@@ -18,7 +21,22 @@ choose = (comp) => {
     let userInput = document.getElementById('userInput');
     choose.addEventListener('click', () => {
         let userNumber = parseInt(userInput.value);
-        if (userNumber === comp) {
+        if(chance===1){
+          let gui=document.getElementById('handle');
+          gui.innerHTML=`<div class="alert alert-danger" role="alert">
+          <h4 class="alert-heading mb-5">OOps !</h4>
+          <p>AWW !! You lose the game. Thanks for playing , please share wiht your friends and others to let them enjoy the game too.</p>
+          <hr>
+          <p class="mb-0">Whenever you need stress relief !<br/>You are Always welcome</p>
+          <button class="btn btn-primary mt-3 mb-2" id="playagain">Try !! again</button>
+        </div>`;
+        alert.style.display='none';
+        let playagain=document.getElementById('playagain');
+        playagain.addEventListener('click',()=>{
+        location.reload();
+        });
+        }
+        else if (userNumber === comp) {
             let gui=document.getElementById('handle');
             gui.innerHTML=`<div class="alert alert-success" role="alert">
             <h4 class="alert-heading mb-5">Well done!</h4>
@@ -47,14 +65,18 @@ choose = (comp) => {
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
           </div>
             `;
+            chance-=1;
+            userChance.innerHTML=`<p>Chance left : <b>${chance}</b>`;
             userInput.value="";
-        }
-        else if (userNumber > comp) {
+          }
+          else if (userNumber > comp) {
             alert.innerHTML=`<div class="alert alert-warning alert-dismissible fade show" role="alert">
             <strong>Uhh !</strong> Computer Choosed less than ${userInput.value}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-          </div>
+            </div>
             `;
+            chance-=1;
+            userChance.innerHTML=`<p>Chance left : <b>${chance}</b>`;
             userInput.value="";
         }
 
